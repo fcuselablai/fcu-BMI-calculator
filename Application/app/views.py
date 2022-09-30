0b62d167152c6c54272f07d839a3cc38e560f40a
yabee <m0830074@mail.fcu.edu.tw>
2022-09-30 10:05:38 +0800

    refactor: add bmi calculator

--- Application/app/views.py
+++ Application/app/views.py
@@ -3,6 +3,7 @@ from django.http import HttpRequest
 from django.template import RequestContext
 from datetime import datetime
 from .forms import BmiForm
+from .utils import bmi_calculator
 
 def home(request):
     bmi = ''
@@ -14,18 +15,6 @@ def home(request):
             form_cd = form.cleaned_data
             h = form_cd.get('height')
             w = form_cd.get('weight')
-            bmi = round(w/(h**2),2)
-            if bmi < 18.5:
-                message = '過輕'
-            elif bmi >= 18.5 and bmi < 24:
-                message = '健康體位'
-            elif bmi >= 24 and bmi < 27:
-                message = '過重'
-            elif bmi >= 27 and bmi < 30:
-                message = '輕度肥胖'
-            elif bmi >= 30 and bmi < 35:
-                message = '中度肥胖'
-            else:
-                message = '重度肥胖'
+            bmi, bmi_means = bmi_calculator(h, w)
     return render(request, 'index.html', locals())
 
